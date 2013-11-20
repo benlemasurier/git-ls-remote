3adef16fb28b554ddf97f56ac925bd7237e38cdb
Ben LeMasurier <ben.lemasurier@gmail.com>
2013-11-20 15:07:06 -0700

    use https git url for test to bypass host key verification

--- test/test.js
+++ test/test.js
@@ -7,7 +7,7 @@ var assert = require('assert'),
 describe('git ls-remote', function() {
   describe('head', function() {
     it('should return the most recent SHA for this repository', function(done) {
-      git.head('git@github.com:benlemasurier/git-ls-remote.git', function(err, result) {
+      git.head('https://github.com/benlemasurier/git-ls-remote.git', function(err, result) {
         if(err) return done(err);
 
         result.should.have.length(40);
